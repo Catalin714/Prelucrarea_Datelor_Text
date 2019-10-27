1f6922fb5ef2594c823c1037cfdcfc7e580f634f
Catalin714 <57073167+Catalin714@users.noreply.github.com>
2019-10-27 21:26:38 +0200

    Team Hagiu Catalin Paracetamol
    Tema

--- WebScrapping.py
+++ WebScrapping.py
@@ -84,7 +84,23 @@ print(compozitie)
 
 
 
+## Paracetamol Hagiu Catalin
+url_medicament1 ='https://comenzi.farmaciatei.ro/farmacie/sistemul-respirator/analgezice/nurofen-plus-24-tablete-reckitt-benckiser-healthcare-p305313'
+page1 = urllib.urlopen(url_medicament1)
 
+soup1 = BeautifulSoup(page1, 'html.parser')
+
+print(soup1)
+##Nume Paracetamol
+name_box1=soup1.find('h1')
+print(name_box1)
+##Pret
+price_box1=soup1.find('div', attrs={"class":'price-box'})
+print(price_box1)
+
+###Compozitie
+compozitie1=soup1.find('div', attrs={"class":'product-desc'})
+print(compozitie1)
 
 
 
